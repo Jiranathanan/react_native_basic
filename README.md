b7ab2bae20720b02d118281b0ade9287d4e711bd
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-03 22:16:04 +0700

    Add useState for CounterScreen

---
+++ README.md
@@ -0,0 +1,7 @@
+### App Setup
+
+# In your terminal run
+`npm install --legacy-peer-deps `
+
+# Run app
+`npm start` or 'expo start`
--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
-    return <View>
-        <Text>Current Count:</Text>
+    const [counter, setCounter] = useState(0);
+    return <View style={{ marginTop: 20}}>
+            <View style={styles.button}>
+                <Button 
+                    title="Increase" 
+                    color='green' 
+                    onPress={ () => {
+                        setCounter(counter + 1);
+                    }}
+                />
+            </View>
+            <View style={styles.button}>
+                <Button 
+                    title="Decrease" 
+                    color='red' 
+                    onPress={ () => {
+                        setCounter(counter - 1)
+                    }}
+                />
+            </View>
+        <Text style={styles.text}>Current Count:</Text>
+        <Text style={styles.text}>{counter}</Text>
     </View>
 }
 
 const styles = new StyleSheet.create({
-
+    text:   { 
+        textAlign: 'center', 
+        fontSize: 24, 
+        marginTop: 20
+    },
+    button: { 
+        marginTop: 10, 
+        marginBottom: 10 
+    }
 });
 
 export default CounterScreen;

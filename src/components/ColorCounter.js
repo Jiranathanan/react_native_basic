99327daa82b6ef5b48831aa4cafa828ffca605c7
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-05 23:28:36 +0700

    Pass Callbacks to children

--- src/components/ColorCounter.js
+++ src/components/ColorCounter.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const ColorCounter = ( { color } ) => {
+const ColorCounter = ( { color, onIncrease, onDecrease } ) => {
     return <View style={{ marginTop: 40 }}>
             <Text style={{ color: color.toLowerCase(), marginBottom: 20 }}>{ color }</Text>
-            <Button title={`Increase ${color}`} color={ color.toLowerCase() } />
-            <Button title={`Decrease ${color}`} color={ color.toLowerCase() } />
+            <Button title={`Increase ${color}`} color={ color.toLowerCase() } onPress={() => onIncrease()} />
+            <Button title={`Decrease ${color}`} color={ color.toLowerCase() } onPress={() => onDecrease()} />
     </View>
 }
 

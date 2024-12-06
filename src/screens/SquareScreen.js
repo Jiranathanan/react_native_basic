20b460324ec17d3f4b8f277bfc24c4ba6fdec4d1
Jiranathanan Siripun <jiranathanan@gmail.com>
2024-12-06 21:28:21 +0700

    Add COLOR_INCREMENT

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import { Text, View, StyleSheet } from 'react-native';
 import ColorCounter from "../components/ColorCounter";
 
+const COLOR_INCREMENT = 25;
+
 const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
 
     return <View>
-        <ColorCounter color="Red" onIncrease={() => setRed(Math.min(red + 15, 255))} onDecrease={() => setRed(Math.max(red-15, 0))} />
-        <ColorCounter color="Green" onIncrease={() => setGreen(Math.min(green + 15, 255))} onDecrease={() => setGreen(Math.max(green - 15, 0))} />
-        <ColorCounter color="Blue" onIncrease={() => setBlue(Math.min(blue+15, 255))} onDecrease={() => setBlue(Math.max(blue-15, 0))} />
+        <ColorCounter color="Red" onIncrease={() => setRed(Math.min(red + COLOR_INCREMENT, 255))} onDecrease={() => setRed(Math.max(red-COLOR_INCREMENT, 0))} />
+        <ColorCounter color="Green" onIncrease={() => setGreen(Math.min(green + COLOR_INCREMENT, 255))} onDecrease={() => setGreen(Math.max(green - COLOR_INCREMENT, 0))} />
+        <ColorCounter color="Blue" onIncrease={() => setBlue(Math.min(blue+COLOR_INCREMENT, 255))} onDecrease={() => setBlue(Math.max(blue-COLOR_INCREMENT, 0))} />
         <View style={{ height: 100, width: 100, 
                         backgroundColor: `rgb( ${red}, ${green}, ${blue})`, 
                         marginHorizontal: 'auto', 
